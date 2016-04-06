058c6ec89ef7df740d2bab486177a32202397ced
jlvivero <josluivivgar@gmail.com>
2016-04-06 13:07:30 -0700

    now able to save database

--- routes/storeDrawing.js
+++ routes/storeDrawing.js
@@ -5,17 +5,36 @@ var Drawing = require('../models/Drawing');
 //this will be the post one, ill make a get as well just to make sure it works
 var storeDrawing = function(req,res){
   var drawing = {};
-  var cords = {};
+  drawing.name = req.body.name;
+  drawing.coord = req.body.coord;
   console.log("name:");
   console.log(req.body.name);
-  console.log("coordinates:");
-  console.log(req.body.cord);
+  console.log("cordinates:");
+  console.log(req.body.coord);
 
-  //change this so that instead of returning this json it saves it on the database
-  res.json({
-    "status": "it worked",
-    "name": drawing.name,
-    "coordinates": drawing.coordinates
+  Drawing.findOne({name: drawing.name}, function(err,draw){
+    console.log("dbresponse");
+    if(!draw){
+      console.log("new drawing");
+      var draw2 = new Drawing({coordinates: drawing.coord, name: drawing.name});
+      draw2.save(function(err,d){
+        if(err){
+          console.log(err);
+        }
+        else {
+          res.json({
+            "status": "successfully saved",
+            "name": drawing.name
+          });
+        }
+      });
+    }
+    else {
+      res.json({
+        "status": "name already exists",
+        "name": "noName"
+      });
+    }
   });
 }
 
