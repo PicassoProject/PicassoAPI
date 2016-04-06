be66de1bf3b3acb6877f0125055d62244c108ccf
jlvivero <josluivivgar@gmail.com>
2016-04-06 01:12:20 -0700

    more test logs

--- routes/storeDrawing.js
+++ routes/storeDrawing.js
@@ -5,9 +5,13 @@ var Drawing = require('../models/Drawing');
 //this will be the post one, ill make a get as well just to make sure it works
 var storeDrawing = function(req,res){
   var drawing = {};
-  drawing.coordinates = req.body.coordinates;
+  var cords = {};
+  cords = req.body.coordinates;
+  drawing.coordinates = cords;
   drawing.name = req.body.name;
   console.log(drawing.coordinates);
+  console.log("please tell me it works");
+  console.log(cords);
   //change this so that instead of returning this json it saves it on the database
   res.json({
     "status": "it worked",
