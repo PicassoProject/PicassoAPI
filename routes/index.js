b738c06fe6dde413d80d9b13302e9113e4a7c273
jlvivero <josluivivgar@gmail.com>
2016-05-26 12:12:31 -0700

    Add an aditional log on the test route

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ var drawStored = require('./drawStored');
 ***********************************************/
 router.post('/store', storeDrawing);
 router.get('/test',function(req,res){
-  console.log(req.body);
+  console.log("i got to the test");
   res.send(200);
 });
 router.post('/test',function(req,res){
