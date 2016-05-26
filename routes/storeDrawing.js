0129b120066e34c401c270e450cb517ed199ee4b
jlvivero <josluivivgar@gmail.com>
2016-05-26 12:26:02 -0700

    remove more useless console logs

--- routes/storeDrawing.js
+++ routes/storeDrawing.js
@@ -13,10 +13,6 @@ var storeDrawing = function(req,res){
   var drawing = {};
   drawing.name = req.body.name;
   drawing.coord = req.body.coord;
-  //console.log("nameyay:");
-  //console.log(req.body.name);
-  //console.log("cordinates:");
-  //console.log(req.body.coord);
 
   Drawing.findOne({name: drawing.name}, function(err,draw){
     //console.log("dbresponse");
