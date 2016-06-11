15858af0fd607aa495c80a9818e23de6d9336936
jlvivero <josluivivgar@gmail.com>
2016-06-10 17:42:06 -0700

    Add chain promises

--- routes/storeDrawing.js
+++ routes/storeDrawing.js
@@ -4,7 +4,7 @@ var Drawing = require('../models/Drawing');
 var convert = require('../helpers/conversionAlgo');
 var Promise = require('promise');
 var math = require('mathjs');
-
+var
 //this will be the post one, ill make a get as well just to make sure it works
 /*******************************************************************************
 * this will receive a drawing with an unique name
@@ -41,59 +41,56 @@ var storeDrawing = function(req,res){
   });
   transform.then(function(value){
     res.send("successfully saved");
+    console.log("i continued with my promise");
+  })
+  .then(function(value){
     var co = [];
     var co = value.coord;
-    console.log("i continued with my promise");
-    var transform2 = new Promise(function(fulfill,reject){
-      console.log(co.length);
-      for(var i = 0; i < co.length; i++)
-      {
-        //console.log(i);
-        var px = co[i].x;
-        var py = co[i].y;
-        var pz = 0;
-        var l1 = 44.45;
-        var l2 = 165.1;
-        var l3 = 152.4;
-        var q1Value = py/px // if atan(py/px) >= 90 use + on the sqrt or use -
-        var q1Value2 = (44.45)/sqrt((px*px) + (py*py) - (l1*l1))
-        console.log(q1Value2);
-        var Q1 = Math.atan(q1Value) - Math.atan(q1Value2);
-        q3Value2 = ((2*l2*l3)/(px*px+py*py+pz*pz-l1*l1-l2*l2*l3*l3))*((2*l2*l3)/(px*px+py*py+pz*pz-l1*l1-l2*l2*l3*l3)) - 1;
-        q3Value = Math.sqrt(q3Value2) //this should be positive or negative depending on something, idk what yet
-        var Q3 = Math.atan(q3Value);
-        var q2Value = ((-pz*(l2+l3*Math.cos(Q3)) - (l3*Math.sin(Q3)*(py*Math.sin(Q1) + px*Math.cos(Q1))))/((px*Math.cos(Q1)+py*Math.sin(Q1)) * (l3*Math.cos(Q3)+l2) - (pz*l3*Math.sin(Q3))));
-        var Q2 = Math.atan(q2Value);
-        angles[i].angle1 = Q1;
-        angles[i].angle2 = Q2;
-        angles[i].angle3 = Q3;
-        console.log("fml")
+    console.log(co.length);
+    for(var i = 0; i < co.length; i++)
+    {
+      //console.log(i);
+      var px = co[i].x;
+      var py = co[i].y;
+      var pz = 0;
+      var l1 = 44.45;
+      var l2 = 165.1;
+      var l3 = 152.4;
+      var q1Value = py/px // if atan(py/px) >= 90 use + on the sqrt or use -
+      var q1Value2 = (44.45)/sqrt((px*px) + (py*py) - (l1*l1))
+      console.log(q1Value2);
+      var Q1 = Math.atan(q1Value) - Math.atan(q1Value2);
+      q3Value2 = ((2*l2*l3)/(px*px+py*py+pz*pz-l1*l1-l2*l2*l3*l3))*((2*l2*l3)/(px*px+py*py+pz*pz-l1*l1-l2*l2*l3*l3)) - 1;
+      q3Value = Math.sqrt(q3Value2) //this should be positive or negative depending on something, idk what yet
+      var Q3 = Math.atan(q3Value);
+      var q2Value = ((-pz*(l2+l3*Math.cos(Q3)) - (l3*Math.sin(Q3)*(py*Math.sin(Q1) + px*Math.cos(Q1))))/((px*Math.cos(Q1)+py*Math.sin(Q1)) * (l3*Math.cos(Q3)+l2) - (pz*l3*Math.sin(Q3))));
+      var Q2 = Math.atan(q2Value);
+      angles[i].angle1 = Q1;
+      angles[i].angle2 = Q2;
+      angles[i].angle3 = Q3;
+      console.log("fml")
+    }
+    console.log("i finished calculating");
+  })
+  .then(function(value){
+    requestObject = {
+    //this url is the url for the edison route
+      url: "10.4.41.219:3000",
+      form: {
+        draw: value
       }
-      console.log("i finished calculating");
-      fulfill(angles);
-    });
-
-    transform2.then(function(value){
-      requestObject = {
-      //this url is the url for the edison route
-        url: "10.4.41.219:3000",
-        form: {
-          draw: value
-        }
+    }
+    console.log(angles);
+    console.log("second promise");
+    request.post(requestObject, function(err,response,body) {
+      if(err) {
+        console.log(err);
+        return;
       }
-      console.log(angles);
-      console.log("second promise");
-      request.post(requestObject, function(err,response,body) {
-        if(err) {
-          console.log(err);
-          return;
-        }
-        else{
-          console.log("successfully transformed");
-        }
-      });
+      else{
+        console.log("successfully transformed");
+      }
     });
   });
 }
-
 module.exports = storeDrawing;
